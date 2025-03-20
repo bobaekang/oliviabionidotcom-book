f4fca8f47cdd855683115ada6acdea5935eeec3a
bobaekang <bkang.dev@gmail.com>
2025-03-19 19:33:07 -0500

    Initial landing page setup

---
+++ src/const.ts
@@ -0,0 +1,70 @@
+export const links = {
+  bookSampleChapters:
+    "https://docs.google.com/document/d/1pYnLWjTZLSdDvaaExI1n0ogWyriOeD0ScckJP0Qd8Xo/edit?usp=sharing",
+  buyMeACoffee: "https://buymeacoffee.com/oliviapodcasts",
+  links: "https://links.oliviabioni.com/",
+  pilatesTeachersManual: "https://pilatesteachersmanual.oliviabioni.com/",
+  pilatesStudentsManual: "https://pilatesstudentsmanual.oliviabioni.com/",
+  shop: "https://shop.oliviabioni.com/",
+  shopBookCollection: "https://shop.oliviabioni.com/collections/ptm-book",
+  youtube: "https://www.youtube.com/channel/UCOLgATdG1_PDxMiFOuv_gwg",
+  pilatesTeachersManualIg: "https://www.instagram.com/pilatesteachersmanual/",
+  pilatesStudentsManualIg: "https://www.instagram.com/pilatesstudentsmanual/",
+};
+
+const linkClass = "italic underline hover:text-blue-500 focus:text-blue-500";
+export const testimonials = [
+  {
+    name: "Dan Baker",
+    description: `Author of <a href="https://www.amazon.com/dp/0648602001" target="_blank" class="${linkClass}">A Pilates Journey</a> and Teacher Trainer at <a href="https://www.thepilateshive.com.au/" target="_blank" class="${linkClass}">The Pilates Hive</a>`,
+    text: `<em>Pilates Teachers Manual: The Book</em> is an invaluable resource for all Pilates teachers. Filled to the brim with tips and advice, it should be recommended reading for all Pilates teachers.`,
+  },
+  {
+    name: "Cody Jussel",
+    description: `Founder of <a href="https://www.anatomyofpilates.com" target="_blank" class="${linkClass}">Anatomy of Pilates</a>`,
+    text: "Olivia has written the book that I wish I had as a new Pilates instructor. [...] It is an essential for any new Pilates instructor or any instructor that just needs a fresh perspective.",
+  },
+  {
+    name: "Heath Lander",
+    description: `Founder of <a href="https://www.contrologycollective.com.au/" target="_blank" class="${linkClass}">Contrology Collective</a>`,
+    text: "Olivia's book does a brilliant job of walking through the thoughts, feelings, and decisions that lie ahead of the aspiring teacher. She lays out the key competencies of all teachers and provides a balanced, well-informed guide to each.",
+  },
+  {
+    name: "Abby Phelps",
+    description: `Owner and Pilates teacher at <a href="https://lp.clubpilates.com/offer?offer_id=free-intro&clubready_referraltypeid=157046&locations=LAKEVIEW,LINCOLNPARK,LOGANSQUARE,NORTHCENTER,RAVENSWOOD,RIVERNORTH,SOUTHLOOP,WESTLOOP,WICKERPARK" class="${linkClass}">Club Pilates Chicago</a>`,
+    text: "Olivia Bioni is a talented and incredibly knowledgeable Pilates teacher. New (and experienced!) Pilates teachers are so lucky that she's shared her invaluable insights in one place.",
+  },
+  {
+    name: "Micki Price-Havard",
+    description: `Owner of <a href="http://mickiphit.com" target="_blank" class="${linkClass}">MickiPhit Wellness</a>`,
+    text: "<em>Pilates Teacher Manual: The Book</em> is a fantastic resource for aspiring Pilates instructors, offering clear guidance on teaching, cueing, and building a sustainable career.",
+  },
+  {
+    name: "Mara Sievers",
+    description: `Founder of <a href="https://www.pilatesencyclopedia.com/" target="_blank" class="${linkClass}">Pilates Encyclopedia</a>`,
+    text: "Olivia has written a delightful guide on how and why to choose a Pilates teaching career and how to thrive in it.",
+  },
+];
+
+export const footerLinks = [
+  {
+    label: "All my links",
+    href: links.links,
+  },
+  {
+    label: "Buy me a coffee",
+    href: links.buyMeACoffee,
+  },
+  {
+    label: "Pilates Teachers' Manual podcast",
+    href: links.pilatesTeachersManual,
+  },
+  {
+    label: "Pilates Students' Manual podcast",
+    href: links.pilatesStudentsManual,
+  },
+  {
+    label: "Podcast YouTube channel",
+    href: links.youtube,
+  },
+];
